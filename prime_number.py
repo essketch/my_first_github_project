c0f63e1f4659227852d3487bbadebdce028692bd
김범준 <kejiegohome@kakao.com>
2021-12-22 17:19:59 +0900

    Update prime_number.py

--- prime_number.py
+++ prime_number.py
@@ -5,3 +5,13 @@ is_prime_num = True
 for i in range(2, num):
     #2 ~ 입력 숫자 직전까지 돌면서,
     #하나라도 나눠지면 소수가 아니라고 할 예정
+    if num % i ==0:
+        #소수 발견!
+        is_prime_num = False
+#is_prime_num이 True로 남아있으면 소수
+if is_prime_num:
+    print(f'{num}은 소수가 맞습니다.')
+else:
+    print(f'{num}은 소수가 아닙니다.')
+
+    
